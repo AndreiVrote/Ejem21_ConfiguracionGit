2f68acbb2beab1d75292558e640498a5c4f648fc
AndreiVrote <andrei.vrote@gregoriofer.com>
2025-04-08 16:20:08 +0200

    Update Aplicacion.java

--- Ejemplo21_ConfiguracionGit/src/main/java/Aplicacion.java
+++ Ejemplo21_ConfiguracionGit/src/main/java/Aplicacion.java
@@ -5,6 +5,7 @@ public class Aplicacion {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo 2");
+		System.out.println("Hola mundo 3"); //desde el editor de gitHub
 	}
 
 }
